fac7c99300ab90d15136f11679c8624ac2244957
Xaver <xaver@everpress.co>
2024-05-27 14:49:52 +0200

    updated: tested up to

--- mailster-contact-form-7.php
+++ mailster-contact-form-7.php
@@ -1,6 +1,7 @@
 <?php
 /*
 Plugin Name: Mailster Contact Form 7
+Requires Plugins: mailster
 Plugin URI: https://mailster.co/?utm_campaign=wporg&utm_source=wordpress.org&utm_medium=plugin&utm_term=Contact+Form+7
 Description: Create your Signup Forms with Contact Form 7 and allow users to signup to your newsletter
 Version: 1.5.1
@@ -14,6 +15,5 @@ define( 'MAILSTER_CF7_VERSION', '1.5.1' );
 define( 'MAILSTER_CF7_REQUIRED_VERSION', '2.2.10' );
 define( 'MAILSTER_CF7_FILE', __FILE__ );
 
-require_once dirname( __FILE__ ) . '/classes/contactform.class.php';
+require_once __DIR__ . '/classes/contactform.class.php';
 new MailsterCF7();
-
